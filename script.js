1af35834c1eb1cfb66b9c834360942ae2ae54b32
sof11x <sofia.murych@yandex.ru>
2024-06-10 05:24:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -29,12 +29,49 @@ function prevReview() {
 nextBtn.addEventListener('click', nextReview);
 prevBtn.addEventListener('click', prevReview);
 
+//js для модального окна
+document.getElementById('applicationForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Предотвращаем отправку формы по умолчанию
+    const fullname = document.getElementById('fullname').value.trim();
+    const phone = document.getElementById('phone').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const errorMessage = document.getElementById('error-message');
+
+    // Простая проверка данных
+    let isValid = true;
+
+    // Проверка ФИО - должно быть хотя бы два слова
+    if (fullname.split(' ').length < 2) {
+        isValid = false;
+    }
+
+    // Проверка телефона - регулярное выражение на 10-13 цифр, возможен '+' на первом месте
+    const phonePattern = /^\+?\d{10,13}$/;
+    if (!phonePattern.test(phone)) {
+        isValid = false;
+    }
+
+    // Проверка почты - HTML5 проверка типа email
+    const emailPattern = /^[\w-]+@([\w-]+\.)+[\w-]{2,4}$/;
+    if (!emailPattern.test(email)) {
+        isValid = false;
+    }
+
+    if (isValid) {
+        errorMessage.style.display = 'none';
+        this.submit();  // Отправка формы если все данные корректны
+    } else {
+        errorMessage.style.display = 'block';
+    }
+});
+//Открытие модального окна
 document.querySelectorAll(".modal-btn_open").forEach(btn => {
     btn.addEventListener("click", function () {
         document.querySelector(".modal").classList.add("open");
     });
 });
-
+//Закрытие модального окна
 document.querySelector(".modal__close-btn").addEventListener("click", function () {
     document.querySelector(".modal").classList.remove("open");
 });
+
